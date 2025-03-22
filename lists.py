8b19f61acc2cdc06210481dfb468f92184c911ab
Vyness6 <vanessahhope@gmail.com>
2025-03-22 09:07:33 +0300

    week 2 assignment

---
+++ lists.py
@@ -0,0 +1,12 @@
+my_list={}
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(1, 15)
+my_list.extend(40, 50, 60)
+my_list.pop(60)
+my_list.sort(10, 20, 30, 40, 50)
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
